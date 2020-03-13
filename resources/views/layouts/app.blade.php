525e4473ebd83c249f2da0d3021e9d2152b5fb2b
Igor Bulygin <boehpyk@gmail.com>
2020-03-13 19:07:20 +0300

    [FIX] Commented concerts buttons on the top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,44 +35,44 @@
             <img src="{{ asset('assets/az.svg') }}" border="0" alt="Animal ДжаZ logo" class="logo" />
         </div>
 
-        <div class="header-section__concerts">
-            <div class="concerts-title">
-                <div class="title-container">
-                    <div class="time-to-love">
-                        Концерты по заявкам
-                        <div class="shadow"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="concert-item left">
-                <div class="concert-item-container">
-                    <div class="date">08.03</div>
-                    <div class="date-sm">08.03</div>
-                    <div class="place">
-                        Санкт-Петербург, Космонавт
-                    </div>
-                    <a href="https://spb.ponominalu.ru/event/animal-dzhaz" class="tickets" target="_blank">Билеты</a>
-                </div>
-                <a href="https://spb.ponominalu.ru/event/animal-dzhaz" class="tickets-sm" target="_blank">Билеты</a>
-            </div>
-            <div class="space">
-            </div>
-            <div class="concert-item right">
-                <div class="concert-item-container">
-                    <div class="date">
-                        20.03
-                    </div>
-                    <div class="date-sm">
-                        20.03
-                    </div>
-                    <div class="place">
-                        Москва, <span style="white-space: nowrap">ДК Горбунова</span>
-                    </div>
-                    <a href="https://afisha.yandex.ru/moscow/concert/animal-dzhaz-2020-03" class="tickets" target="_blank">Билеты</a>
-                </div>
-                <a href="https://afisha.yandex.ru/moscow/concert/animal-dzhaz-2020-03" target="_blank" class="tickets-sm">Билеты</a>
-            </div>
-        </div>
+        {{--<div class="header-section__concerts">--}}
+            {{--<div class="concerts-title">--}}
+                {{--<div class="title-container">--}}
+                    {{--<div class="time-to-love">--}}
+                        {{--Концерты по заявкам--}}
+                        {{--<div class="shadow"></div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="concert-item left">--}}
+                {{--<div class="concert-item-container">--}}
+                    {{--<div class="date">08.03</div>--}}
+                    {{--<div class="date-sm">08.03</div>--}}
+                    {{--<div class="place">--}}
+                        {{--Санкт-Петербург, Космонавт--}}
+                    {{--</div>--}}
+                    {{--<a href="https://spb.ponominalu.ru/event/animal-dzhaz" class="tickets" target="_blank">Билеты</a>--}}
+                {{--</div>--}}
+                {{--<a href="https://spb.ponominalu.ru/event/animal-dzhaz" class="tickets-sm" target="_blank">Билеты</a>--}}
+            {{--</div>--}}
+            {{--<div class="space">--}}
+            {{--</div>--}}
+            {{--<div class="concert-item right">--}}
+                {{--<div class="concert-item-container">--}}
+                    {{--<div class="date">--}}
+                        {{--20.03--}}
+                    {{--</div>--}}
+                    {{--<div class="date-sm">--}}
+                        {{--20.03--}}
+                    {{--</div>--}}
+                    {{--<div class="place">--}}
+                        {{--Москва, <span style="white-space: nowrap">ДК Горбунова</span>--}}
+                    {{--</div>--}}
+                    {{--<a href="https://afisha.yandex.ru/moscow/concert/animal-dzhaz-2020-03" class="tickets" target="_blank">Билеты</a>--}}
+                {{--</div>--}}
+                {{--<a href="https://afisha.yandex.ru/moscow/concert/animal-dzhaz-2020-03" target="_blank" class="tickets-sm">Билеты</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
         {{--<section class="header-section__top-links">--}}
         {{--<ul class="top-links__container">--}}
