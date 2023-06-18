b705b1f695caad2d5194ac0ff3d3a483e2693a41
boehpyk <boehpyk@gmail.com>
2023-06-18 11:30:21 +0200

    [FIX] Fixed contacts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -109,55 +109,14 @@
                             <h3 class="mdc-list-group__subheader">Booking:</h3>
                             <ul class="mdc-list mdc-list--two-line mdc-list--non-interactive">
                                 <li class="contacts__item mdc-list-item">
-                                    <span class="contacts__avatar mdc-list-item__graphic">
-                                      <a target="_blank" href="https://vk.com/zarankin">
-                                        <img src="{{ asset('assets/zar.jpg') }}" alt="Александр Заранкин">
-                                      </a>
-                                    </span>
-                                    <span class="mdc-list-item__text">
-                                      <a target="_blank" href="https://vk.com/zarankin">Александр Заранкин</a>
-                                      <span class="mdc-list-item__secondary-text">
-                                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M0 0h24v24H0z" fill="none"/>
-                                            <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
-                                        </svg>
-                                        <a target="_blank" href="tel:+79110922882">+7 (911) 092-28-82</a>
-                                        <br>
-                                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
-                                            <path d="M0 0h24v24H0z" fill="none"/>
-                                        </svg>
-                                        <a target="_blank" href="mailto:zarankin@bk.ru">zarankin@bk.ru</a>
-                                      </span>
-                                    </span>
+                                    <a target="_blank" href="mailto:booking@animaljazz.com">booking@animaljazz.com</a>
                                 </li>
                             </ul>
                             <hr class="mdc-list-divider mdc-list-divider--inset" role="separator">
-                            <h3 class="mdc-list-group__subheader">PR:</h3>
+                            <h3 class="mdc-list-group__subheader">Tickets:</h3>
                             <ul class="mdc-list mdc-list--two-line mdc-list--non-interactive">
                                 <li class="contacts__item mdc-list-item">
-                                    <span class="contacts__avatar mdc-list-item__graphic">
-                                      <a target="_blank" href="https://vk.com/marygoldpr">
-                                        <img src="{{ asset('assets/mar.jpg') }}" alt="">
-                                      </a>
-                                    </span>
-                                    <span class="mdc-list-item__text">
-                                      <a target="_blank" href="https://vk.com/marygoldpr"> Мария Минина</a>
-                                      <span class="mdc-list-item__secondary-text">
-                                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M0 0h24v24H0z" fill="none"/>
-                                            <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
-                                        </svg>
-                                        <a target="_blank" href="tel:+79032165576">+7 (903) 216-55-76</a>
-                                        <br>
-                                            <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
-                                            <path d="M0 0h24v24H0z" fill="none"/>
-                                        </svg>
-
-                                        <a target="_blank" href="mailto:marygoldpr@gmail.com">marygoldpr@gmail.com</a>
-                                      </span>
-                                    </span>
+                                    <a target="_blank" href="mailto:bilet@animaljazz.com">bilet@animaljazz.com</a>
                                 </li>
                             </ul>
                         </div>
